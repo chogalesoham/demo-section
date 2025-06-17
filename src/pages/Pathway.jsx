e1f46af8a440d14acef69d3112f4d738ed05df19
soham_chogale <chogalesoham74@gmail.com>
2025-06-17 15:25:23 +0530

    complited

--- src/pages/Pathway.jsx
+++ src/pages/Pathway.jsx
@@ -2,29 +2,165 @@ import React from "react";
 
 const Pathway = () => {
   return (
-    <div className="relative w-[658px] h-[650px] mx-auto mt-6">
-      <img src="/images/startbutton3.svg" alt="Pathway" />
-      <div className=" absolute top-[55px] left-[70px]">
-        <img src="/images/questionBtn.svg" alt="" />
-      </div>
-      <div className=" absolute top-[203px] left-[-20px]">
-        <img src="/images/questionBtn.svg" alt="" />
+    <div className=" flex flex-col items-center justify-center bg-[#F5F5F5] gap-30 py-10">
+      <div className="relative w-[658px] h-[650px] mx-auto mt-6">
+        <img src="/images/Ellipse 22.svg" alt="Pathway" />
+        <div className=" absolute top-[-60px] left-[278px]">
+          <img src="/images/Group 383.svg" alt="" />
+        </div>
+        <div className=" absolute top-[13px] left-[92px]">
+          <img src="/images/questionBtn.svg" alt="" />
+        </div>
+        <div className=" absolute top-[181px] left-[-31px]">
+          <img src="/images/questionBtn.svg" alt="" />
+        </div>
+
+        <div className=" absolute top-[370px] left-[-28px]">
+          <img src="/images/questionBtn.svg" alt="" />
+        </div>
+        <div className=" absolute top-[512px] left-[72px]">
+          <img src="/images/questionBtn.svg" alt="" />
+        </div>
+        <div className=" absolute top-[590px] left-[233px]">
+          <img src="/images/questionBtn.svg" alt="" />
+        </div>
+        <div className=" absolute top-[153px] left-[185px]">
+          <img src="/images/Group 502.svg" alt="" />
+        </div>
+
+        <div className=" absolute top-[198px] left-[440px]">
+          <img src="/images/Group 384.svg" alt="" />
+          <img src="/images/Group 385.svg" alt="" />
+        </div>
+
+        <div className=" absolute top-[538px] left-[382px]">
+          <img src="/images/trophyBtn.svg" alt="" />
+        </div>
       </div>
 
-      <div className=" absolute top-[339px] left-[-60px]">
-        <img src="/images/treasureBtn.svg" alt="" />
+      <img src="/images/Group 114.svg" alt="Pathway" />
+
+      <div className="relative w-[658px] h-[650px] mx-auto mt-6">
+        <img src="/images/Ellipse 22.svg" alt="Pathway" />
+        <div className=" absolute top-[-60px] left-[278px]">
+          <img src="/images/Group 383.svg" alt="" />
+        </div>
+        <div className=" absolute top-[36px] left-[70px]">
+          <img src="/images/questionBtn.svg" alt="" />
+        </div>
+        <div className=" absolute top-[203px] left-[-20px]">
+          <img src="/images/questionBtn.svg" alt="" />
+        </div>
+
+        <div className=" absolute top-[339px] left-[-60px]">
+          <img src="/images/treasureBtn.svg" alt="" />
+        </div>
+        <div className=" absolute top-[531px] left-[70px]">
+          <img src="/images/questionBtn.svg" alt="" />
+        </div>
+        <div className=" absolute top-[590px] left-[233px]">
+          <img src="/images/questionBtn.svg" alt="" />
+        </div>
+        <div className=" absolute top-[153px] left-[185px]">
+          <img src="/images/Group 502.svg" alt="" />
+        </div>
+
+        <div className=" absolute top-[198px] left-[440px]">
+          <img src="/images/Group 384.svg" alt="" />
+          <img src="/images/Group 385.svg" alt="" />
+        </div>
+        <div className=" absolute top-[538px] left-[382px]">
+          <img src="/images/trophyBtn.svg" alt="" />
+        </div>
       </div>
-      <div className=" absolute top-[531px] left-[54px]">
-        <img src="/images/questionBtn.svg" alt="" />
+
+      <img src="/images/Group 114.svg" alt="Pathway" />
+
+      <div className="relative w-[658px] h-[650px] mx-auto mt-6">
+        <img src="/images/Ellipse 22.svg" alt="Pathway" />
+        <div className=" absolute top-[-60px] left-[278px]">
+          <img src="/images/Group 383.svg" alt="" />
+        </div>
+        <div className=" absolute top-[32px] left-[70px]">
+          <img src="/images/questionBtn.svg" alt="" />
+        </div>
+        <div className=" absolute top-[203px] left-[-20px]">
+          <img src="/images/questionBtn.svg" alt="" />
+        </div>
+
+        <div className=" absolute top-[339px] left-[-60px]">
+          <img src="/images/treasureBtn.svg" alt="" />
+        </div>
+        <div className=" absolute top-[562px] left-[150px]">
+          <img src="/images/questionBtn.svg" alt="" />
+        </div>
+
+        <div className=" absolute top-[153px] left-[185px]">
+          <img src="/images/Group 502.svg" alt="" />
+        </div>
+
+        <div className=" absolute top-[198px] left-[440px]">
+          <img src="/images/Group 384.svg" alt="" />
+          <img src="/images/Group 385.svg" alt="" />
+        </div>
+        <div className=" absolute top-[538px] left-[382px]">
+          <img src="/images/trophyBtn.svg" alt="" />
+        </div>
       </div>
-      <div className=" absolute top-[590px] left-[233px]">
-        <img src="/images/questionBtn.svg" alt="" />
+
+      <img src="/images/Group 114.svg" alt="Pathway" />
+
+      <div className="relative w-[658px] h-[650px] mx-auto mt-6">
+        <img src="/images/Ellipse 22.svg" alt="Pathway" />
+        <div className=" absolute top-[-60px] left-[278px]">
+          <img src="/images/Group 383.svg" alt="" />
+        </div>
+        <div className=" absolute top-[80px] left-[31px]">
+          <img src="/images/questionBtn.svg" alt="" />
+        </div>
+
+        <div className=" absolute top-[339px] left-[-60px]">
+          <img src="/images/treasureBtn.svg" alt="" />
+        </div>
+        <div className=" absolute top-[562px] left-[150px]">
+          <img src="/images/questionBtn.svg" alt="" />
+        </div>
+
+        <div className=" absolute top-[153px] left-[185px]">
+          <img src="/images/Group 502.svg" alt="" />
+        </div>
+
+        <div className=" absolute top-[198px] left-[440px]">
+          <img src="/images/Group 384.svg" alt="" />
+          <img src="/images/Group 385.svg" alt="" />
+        </div>
+        <div className=" absolute top-[538px] left-[382px]">
+          <img src="/images/trophyBtn.svg" alt="" />
+        </div>
       </div>
-      <div className=" absolute top-[153px] left-[260px]">
-        <img src="/images/Group 502.svg" alt="" />
-      </div>
-      <div className=" absolute top-[538px] left-[382px]">
-        <img src="/images/trophyBtn.svg" alt="" />
+
+      <img src="/images/Group 114.svg" alt="Pathway" />
+
+      <div className="relative w-[658px] h-[650px] mx-auto mt-6">
+        <img src="/images/Ellipse 22.svg" alt="Pathway" />
+        <div className=" absolute top-[-60px] left-[278px]">
+          <img src="/images/Group 383.svg" alt="" />
+        </div>
+        <div className=" absolute top-[275px] left-[-37px]">
+          <img src="/images/questionBtn.svg" alt="" />
+        </div>
+
+        <div className=" absolute top-[153px] left-[185px]">
+          <img src="/images/Group 502.svg" alt="" />
+        </div>
+
+        <div className=" absolute top-[198px] left-[440px]">
+          <img src="/images/Group 384.svg" alt="" />
+          <img src="/images/Group 385.svg" alt="" />
+        </div>
+        <div className=" absolute top-[538px] left-[382px]">
+          <img src="/images/trophyBtn.svg" alt="" />
+        </div>
       </div>
     </div>
   );
